0b6fe8a1d9a9879eaa9b9a0ff0e60f425973c539
benji1477 <116378398+benji1477@users.noreply.github.com>
2024-05-01 14:27:35 +0200

    Update connexion_traitement.php
    Correctif de plusieurs bug : 
     - les redirections fonctionnent
     - la connexion avec l'IDRH et le mdp fonctionne
     - optimisation avec un foreach
     - suppression des balises HTML (ne servent pas en traitement)
    
    / ! \ si on envoit formulaire vide, $_POST est "set" avec la valeur "", une chaine nulle. On doit donc tester le contenu de $_POST

--- intranet/connexion_traitement.php
+++ intranet/connexion_traitement.php
@@ -1,50 +1,45 @@
 <?php
 session_start();
-
-?>
-<!DOCTYPE html>
-
-<html>
-<body>
-
-</br>
-<?php
 //recupère le contenu du fichier users.json dans un tableau de tableau
 $users_list = json_decode(file_get_contents('../data/users.json'), true);
+//variable de controle
+$controle = 1;
 
-
-if(isset($_POST["identifiant"])){
-
-    for ($i=0; $i< count($users_list); $i++){
-            if(in_array($_POST["identifiant"], $users_list[$i]) and password_verify($_POST["mdp"],$users_list[$i]["mdp"],)){
-
-                $_SESSION["name"] = $users_list[$i]["name"];
-                $_SESSION["surname"] = $users_list[$i]["surname"];
-                $_SESSION["idrh"] = $users_list[$i]["idrh"]; //=$_POST["identifiant"]
-                $_SESSION["service"] = $users_list[$i]["service"];
-                $_SESSION["telephone"] = $users_list[$i]["telephone"];
-                $_SESSION["mail"] = $users_list[$i]["mail"];
-                $_SESSION["role"] = $users_list[$i]["role"];
+if(isset($_POST["submit"])){
+    if($_POST["identifiant"] != "" && $_POST["mdp"] != ""){
+        foreach($users_list as $user){
+            if($_POST["identifiant"] == $user['idrh'] && password_verify($_POST["mdp"],$user["mdp"])){
+                $controle = 0;
+                $_SESSION["name"] = $user["name"];
+                $_SESSION["surname"] = $user["surname"];
+                $_SESSION["idrh"] = $user["idrh"]; //=$_POST["identifiant"]
+                $_SESSION["service"] = $user["service"];
+                $_SESSION["telephone"] = $user["telephone"];
+                $_SESSION["mail"] = $user["mail"];
+                $_SESSION["role"] = $user["role"];
                 //ne pas mettre mdp dans la session
 
                 http_response_code(301);
-                header('accueil.php');
+                //header('location:/SAE203/intranet/accueil.php'); créer la page acceuil de l'intranet ! 
                 break;
                 
             }
     }
-
-    if (!isset($_SESSION["idrh"])){
+}
+else{
     http_response_code(301);
-    header('connexion_traitement.php?error=mdp'); //si malgré le traitement, il n'y a rien dans la sessions, c'est que le mdp ne correspond pas ou que l'identifiant est erroné alors on renvoie une erreur
+    header('location:/SAE203/intranet/connexion_intranet.php?error=nopost'); //si il n'y a rien dans le $_POST, on renvoie une erreur
+    // afficher une erreur "Tout les champs ne sont pas complet ! " 
+}
+
+
+if ($controle == 1){ //si la session est vide ==> mauvais mdp ou IDRH
+    http_response_code(301);
+    header('location:/SAE203/intranet/connexion_intranet.php?error=mdp'); 
+    //afficher une erreur " Identifiant ou mot de passe incorrect ! " 
     }
-}
-else {
-    http_response_code(301);
-    header('connexion_traitement.php?error=nopost'); //si il n'y a rien dans le $_POST, on renvoie une erreur
-}
 
+}
 ?>
 
-</body>
-</html>
+
