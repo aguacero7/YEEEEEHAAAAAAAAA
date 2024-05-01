ba5cc2e0ce7e54de81f5d958ebe5a5a3eb02e777
Florianseagull <166707499+Florianseagull@users.noreply.github.com>
2024-05-01 10:42:20 +0200

    Create connexion_traitement.php
    A voir si modifs necessaire afin de bien lier avec connexion_intranet.php

---
+++ intranet/connexion_traitement.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+
+<html>
+<body>
+
+</br>
+<?php
+//recupère le contenu du fichier users.json dans un tableau de tableau
+$users_list = json_decode(file_get_contents('../data/users.json'), true);
+
+
+if(isset($_POST["identifiant"])){
+
+    for ($i=0; $i< count($users_list); $i++){
+            if(in_array($_POST["identifiant"], $users_list[$i]) and password_verify($_POST["mdp"],$users_list[$i]["mdp"],)){
+
+                $_SESSION["name"] = $users_list[$i]["name"];
+                $_SESSION["surname"] = $users_list[$i]["surname"];
+                $_SESSION["idrh"] = $users_list[$i]["idrh"]; //=$_POST["identifiant"]
+                $_SESSION["service"] = $users_list[$i]["service"];
+                $_SESSION["telephone"] = $users_list[$i]["telephone"];
+                $_SESSION["mail"] = $users_list[$i]["mail"];
+                $_SESSION["role"] = $users_list[$i]["role"];
+                //ne pas mettre mdp dans la session
+
+                http_response_code(301);
+                header('accueil.php');
+                break;
+                
+            }
+    }
+
+    if (!isset($_SESSION["idrh"])){
+    http_response_code(301);
+    header('connexion_traitement.php?error=nosession'); //si malgré le traitement, il n'y a rien dans la sessions,alors on renvoie une erreur
+    }
+}
+else {
+    http_response_code(301);
+    header('connexion_traitement.php?error=nopost'); //si il n'y a rien dans le $_POST, on renvoie une erreur
+}
+
+?>
+
+</body>
+</html>
