36910f9e28889ca0aa0de078aec936985b3b7f05
Florianseagull <166707499+Florianseagull@users.noreply.github.com>
2024-05-05 13:49:12 +0200

    Update deconnexion_intranet.php
    //a voir si on ne redirige pas vers la page d'acceuil de l'intranet

--- SAE203/intranet/deconnexion_intranet.php
+++ SAE203/intranet/deconnexion_intranet.php
@@ -2,5 +2,10 @@
 session_start();
 session_unset();
 session_destroy();
+
+//a voir si on ne redirige pas vers la page d'acceuil de l'intranet
+http_response_code(301);
 header('location:/SAE203/intranet/connexion_intranet.php');
+
+
 ?>
