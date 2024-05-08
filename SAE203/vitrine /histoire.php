e29c414ba3855bb110267ac0c10077f9d7bad23f
Theboubou <166707392+Theboubou@users.noreply.github.com>
2024-05-08 14:12:03 +0200

    Update histoire.php changement du head

--- SAE203/vitrine /histoire.php
+++ SAE203/vitrine /histoire.php
@@ -2,7 +2,25 @@
     include('../functions.php');
     ?>
     <?php
-    echo vitrine_head();
+     echo "
+    <!DOCTYPE html>
+    <html lang='fr'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <link rel='stylesheet' href='./CSS/timeline.css'>
+        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css'>
+        <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>
+        <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>
+		<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css'>
+		<link rel='stylesheet' href='./CSS/colors.css'>
+        <title>Document</title>
+    </head>
+    <body>
+        
+    </body>
+    </html>
+    ";
     ?>
 
 
