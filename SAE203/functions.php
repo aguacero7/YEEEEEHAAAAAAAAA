448802f20bce476c528b8b660894d803e186c93d
Mercure <153399795+Mercure95@users.noreply.github.com>
2024-05-09 20:12:25 +0200

    Update functions.php
    changement du titre de la page (Equarris --> Equaris)

--- SAE203/functions.php
+++ SAE203/functions.php
@@ -265,10 +265,8 @@ function vitrine_head(){
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Equarris</title>
-            <!-- Utilisez uniquement Bootstrap 4.5.2 -->
+            <title>Equaris</title>
             <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-            <!-- Utilisez uniquement jQuery 3.5.1.slim.min.js pour Bootstrap 4.5.2 -->
             <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
