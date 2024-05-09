c64136ca98a67e845e235691cd1e54e493dc2df2
Theboubou <166707392+Theboubou@users.noreply.github.com>
2024-05-09 13:18:34 +0200

    Update equaris.php ajout du contenu de la page

--- SAE203/vitrine /equaris.php
+++ SAE203/vitrine /equaris.php
@@ -1,1 +1,16 @@
+<?php
+include('../functions.php');
+?>
+<?php
+echo vitrine_head();
+?>
+
+
+<body>
+<?php
+echo vitrine_navbar();
+echo vitrine_equaris_body();
+echo vitrine_footer();
+?>
+</body>
 
