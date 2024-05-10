c6f17416d3912058c863f4dd3474ad34fe8a8cb1
Mercure <153399795+Mercure95@users.noreply.github.com>
2024-05-10 10:33:47 +0200

    ajout de offres.php
    WIP

---
+++ SAE203/vitrine/offres.php
@@ -0,0 +1,15 @@
+<?php
+include('../functions.php');
+?>
+<?php
+echo vitrine_head();
+?>
+
+
+<body>
+<?php
+echo vitrine_navbar();
+echo vitrine_offres_body();
+echo vitrine_footer();
+?>
+</body>
