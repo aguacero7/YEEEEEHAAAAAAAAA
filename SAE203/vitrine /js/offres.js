211e4c479f9f1b7f9e02edb3f5668cdcb9deefb3
Mercure <153399795+Mercure95@users.noreply.github.com>
2024-05-11 11:22:12 +0200

    Update offres.js
    offres.js  à jour

--- SAE203/vitrine /js/offres.js
+++ SAE203/vitrine /js/offres.js
@@ -1,1 +1,11 @@
-
+document.querySelectorAll(".card").forEach(function(card) { // séléctionne tous les éléments HTML avec la classe card
+    card.addEventListener("mouseover", function() {        // ajoute un écouteur d\'event qui déclenche une fonction
+        var text = this.querySelector(".card-text");      // on stocke la partie texte dans la variable text
+        if (text) text.style.display = "block";          // si text n\'est pas vide, onb affiche le texte
+    });
+    
+    card.addEventListener("mouseout", function() {     // pour détecter quand la souris quitte la card
+        var text = this.querySelector(".card-text");  // on stocke la partie texte dans la variable text
+        if (text) text.style.display = "none";       // si text n\'est pas vide, onb affiche pas le texte
+    });
+});
