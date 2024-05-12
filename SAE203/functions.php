ba6cd9c5c922700ca229055b4d8cd4fdee42851f
benji1477 <116378398+benji1477@users.noreply.github.com>
2024-05-12 16:02:44 +0200

    Réctification d'un bug occurrent sur le footer
    Le chemin spécifié était incorrect

--- SAE203/functions.php
+++ SAE203/functions.php
@@ -275,7 +275,7 @@ function vitrine_head(){
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-    		<link rel="stylesheet" href="./CSS/colors.css">
+    		<link rel="stylesheet" href="/SAE203/vitrine/CSS/colors.css">
         </head>
     ';
 }
